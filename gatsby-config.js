9c0e70d254bee70475277a86e9ff9056ef4922a3
Janique-ka John <janique-kajohn@Janique-kas-MacBook-Pro.local>
2021-04-24 20:54:19 +0200

    styling improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Janique-ka John personal website',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is the personal website of Janique-ka John.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
